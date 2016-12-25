963c8562905c3d4fdc2347a4ae1248e6054b443f
SuperHaker <amrits3624@gmail.com>
2016-12-25 21:01:32 +0530

    Updating

--- app/src/main/java/android/example/com/fictionary/MainActivity.java
+++ app/src/main/java/android/example/com/fictionary/MainActivity.java
@@ -5,9 +5,9 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
-import android.support.v4.widget.SimpleCursorAdapter;
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
@@ -29,19 +29,15 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the TextView which will be populated with the Dictionary ContentProvider data.
         ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
 
-        // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
-        // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
-        // -- YOUR CODE BELOW HERE -- //
 
-        // Set the Adapter to fill the standard two_line_list_item layout with data from the Cursor.
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.two_line_list_item, cursor,COLUMNS_TO_BE_FOUND, LAYOUT_ITEMS_TO_FILL,0);
+
 
         // Don't forget to attach the adapter to the ListView
         dictListView.setAdapter(adapter);
