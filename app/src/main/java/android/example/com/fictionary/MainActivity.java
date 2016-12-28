188e1bc14b11421c791f3d0d3139058f1338fb01
SuperHaker <amrits3624@gmail.com>
2016-12-29 00:33:17 +0530

    Context Menu

--- app/src/main/java/android/example/com/fictionary/MainActivity.java
+++ app/src/main/java/android/example/com/fictionary/MainActivity.java
@@ -7,7 +7,12 @@ import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,20 +26,43 @@ public class MainActivity extends ActionBarActivity {
             R.id.frequency
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
+
+
         ContentResolver resolver = getContentResolver();
 
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
 
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),R.layout.listview_layout, cursor,COLUMNS_TO_BE_FOUND, LAYOUT_ITEMS_TO_FILL,0);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.listview_layout, cursor, COLUMNS_TO_BE_FOUND, LAYOUT_ITEMS_TO_FILL, 0);
 
 
         dictListView.setAdapter(adapter);
+        registerForContextMenu(dictListView);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        ListView lv = (ListView) v;
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        Cursor cursor = (Cursor) lv.getItemAtPosition(info.position);
+        menu.add(0, v.getId(), 0, "Delete");
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle()=="Delete")Toast.makeText(getApplicationContext(), "Delete Clicked", Toast.LENGTH_LONG).show();
+        return true;
     }
 }
+
+
