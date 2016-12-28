aaf8de27323b4958e8f4ed2f48e9a0a9855e908a
SuperHaker <amrits3624@gmail.com>
2016-12-29 00:37:48 +0530

    Removing shitstuff and going to sleep

--- app/src/main/java/android/example/com/fictionary/MainActivity.java
+++ app/src/main/java/android/example/com/fictionary/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends ActionBarActivity {
             Words.WORD, Words.FREQUENCY
     };
 
-    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[] {
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[]{
             R.id.word,
             R.id.frequency
     };
@@ -31,36 +31,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
-
-
         ContentResolver resolver = getContentResolver();
-
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
-
-
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.listview_layout, cursor, COLUMNS_TO_BE_FOUND, LAYOUT_ITEMS_TO_FILL, 0);
-
-
         dictListView.setAdapter(adapter);
         registerForContextMenu(dictListView);
     }
-
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         ListView lv = (ListView) v;
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
         Cursor cursor = (Cursor) lv.getItemAtPosition(info.position);
         menu.add(0, v.getId(), 0, "Delete");
     }
 
-
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if(item.getTitle()=="Delete")Toast.makeText(getApplicationContext(), "Delete Clicked", Toast.LENGTH_LONG).show();
+        if (item.getTitle() == "Delete")
+            Toast.makeText(getApplicationContext(), "Delete Clicked", Toast.LENGTH_LONG).show();
         return true;
     }
 }
